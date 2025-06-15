1ea1c21132759fe48b1a6719cebe8980d3d96656
theBappy <abappy575@gmail.com>
2025-06-15 23:35:37 +0600

    social sign up using google and github implementaion

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -2,9 +2,13 @@
 
 import Image from "next/image"
 
-const Avatar = () => {
+interface AvatarProps {
+  src: string | null | undefined;
+}
+
+const Avatar = ({src}:AvatarProps) => {
   return (
-    <Image src="/images/placeholder.jpg" className="rounded-full" width={30} height={30} alt="avatar" />
+    <Image src={src || "/images/placeholder.jpg"} className="rounded-full" width={30} height={30} alt="avatar" />
   )
 }
 
