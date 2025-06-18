2fd82a4e457a6a2514aa14d3c9a0d15f532758c6
theBappy <abappy575@gmail.com>
2025-06-18 14:14:27 +0600

    Trips routing with own reservation list

--- src/app/listings/[listingId]/page.tsx
+++ src/app/listings/[listingId]/page.tsx
@@ -6,7 +6,7 @@ import ListingClient from "./ListingClient";
 import getReservations from "@/app/actions/getReservations";
 
 interface IParams {
-  listingId?: string;
+  listingId: string;
 }
 
 const ListingPage = async ({ params }: { params: IParams }) => {
